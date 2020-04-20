c7558ede28149d1fef08234833865a7ed68e83ee
likithnaik <47376008+likithnaik@users.noreply.github.com>
2020-04-20 21:41:52 +0530

    Update index.php

--- index.php
+++ index.php
@@ -4,10 +4,6 @@
 <select name="from_currency">
 <option value="INR">Indian Rupee</option>
 <option value="USD" selected="1">US Dollar</option>
-<option value="AUD">Australian Dollar</option>
-<option value="EUR">Euro</option>
-<option value="EGP">Egyptian Pound</option>
-<option value="CNY">Chinese Yuan</option>
 </select>
  <label>Amount</label>
 <input type="text" placeholder="Currency" name="amount" id="amount" />
@@ -15,10 +11,6 @@
 <select name="to_currency">
 <option value="INR" selected="1">Indian Rupee</option>
 <option value="USD">US Dollar</option>
-<option value="AUD">Australian Dollar</option>
-<option value="EUR">Euro</option>
-<option value="EGP">Egyptian Pound</option>
-<option value="CNY">Chinese Yuan</option>
 </select>
   <button type="submit" name="convert" id="convert" class="btn btn-default">Convert</button>
 </div>
